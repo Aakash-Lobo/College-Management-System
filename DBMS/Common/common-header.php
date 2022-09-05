c49ee9f351244a2492930aa19307957814c9829e
Aakash-Lobo <96019345+Aakash-Lobo@users.noreply.github.com>
2022-09-06 00:21:43 +0530

    Add files via upload

---
+++ DBMS/Common/common-header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <!-- css style goes here -->
+
+    <link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+
+    <!-- css style go to end here -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+</body>
+
+</html>
